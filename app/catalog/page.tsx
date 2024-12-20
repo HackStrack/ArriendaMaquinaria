96976b929bbc1616c6e51003e17c12fc35be205b
MaximilianoVorphal <maximilianovorphal@gmail.com>
2024-12-20 17:47:32 -0300

    ola

--- app/catalog/page.tsx
+++ app/catalog/page.tsx
@@ -10,6 +10,8 @@ import Footer from '@/components/sections/footer-section';
 export default function CatalogPage() {
   const { filteredProducts, filters, updateFilters } = useProductFilters();
 
+
+  
   return (
     <>
       <Navbar />
